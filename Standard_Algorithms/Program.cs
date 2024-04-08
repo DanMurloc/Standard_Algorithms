a0bccb6d68aed4d65ec8a92e8f2489ec9592db1e
DanMurloc <strokalov1@gmail.com>
2024-04-08 13:59:03 +0300

    Добавьте файлы проекта.

---
+++ Standard_Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Standard_Algorithms;
+
+Console.WriteLine("Hello, World!");
+
+int[] arr = {8,4,7,1,2};
+
+Sorting_standard a = new Sorting_standard();
+
+a.RevBubbleInvSort(ref arr);
+
+for (int i = 0; i < arr.Length; i++)
+    Console.Write(arr[i] + " ");
+Console.WriteLine("\n##########################################");
+Console.WriteLine(a.Factorial(10));
+Console.WriteLine(a.FactorialFor(10));
+Console.WriteLine("##########################################");
+Console.WriteLine(a.Fibonachi(5));
+Console.WriteLine(a.FibonachiFor(5));
+
+Console.WriteLine("##########################################");
---
+++ Standard_Algorithms/Sorting standard.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard_Algorithms
+{
+    public class Sorting_standard
+    {
+
+        public void BubbleSort(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length-i-1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+        public void RevBubbleSort(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+        public void BubbleInvSort(ref int[] arr)
+        {
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+        public void RevBubbleInvSort(ref int[] arr)
+        {
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public int Factorial(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+
+        public int FactorialFor(int n)
+        {
+            if (n == 1 || n == 0) return 1;
+            int Fsum = 1;
+            for (int i = n; i > 0; i--)
+            {
+                Fsum *= i;
+            }
+            return Fsum;
+        }
+
+        public int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+
+        public int FibonachiFor(int n)
+        {
+            int result = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = result;
+                result = b;
+                b += tmp;
+            }
+
+            return result;
+        }
+    }
+}
